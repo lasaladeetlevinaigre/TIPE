d9d89653b1e88bf7b897cb90f60b1462b01fdbdb
lasaladeetlevinaigre <133405015+lasaladeetlevinaigre@users.noreply.github.com>
2023-05-12 21:14:21 +0200

    Add files via upload

--- plot.py
+++ plot.py
@@ -1,65 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
-"""
-^ Uy
-|
-|
-|
-|
-O--------> Ux
-Uz vers nous
-"""
-
-
-g = 9.81
-y_mur = 10 # en metres à partir du JOUEUR
-
-
-# Définition des fonctions pour chaque partie de la trajectoire
-def part1(t, v0):
-    x = v0[0] * t
-    y = v0[1] * t
-    z = v0[2] * t**2
-    return x, y, z
-
-def part2(t, v0):
-    x = v0[0] + v0[0] * t
-    y = v0[1] + v0[1] * t
-    z = v0[2] + v0[2] * t - 0.5 * 9.81 * t**2
-    return x, y, z
-
-def part3(t, v0):
-    x = v0[0] + v0[0] * t + 0.5 * 2 * t**2
-    y = v0[1] + v0[1] * t
-    z = v0[2] + v0[2] * t - 0.5 * 9.81 * t**2
-    return x, y, z
-
-# Vecteur vitesse initial
-v0 = [10, 5, 3]
-
-# Création de la liste des temps pour chaque partie de la trajectoire
-t1 = np.linspace(0, 1, 50)
-t2 = np.linspace(1, 2, 50)
-t3 = np.linspace(2, 3, 50)
-
-# Création des listes des coordonnées x, y et z pour chaque partie de la trajectoire
-x1, y1, z1 = zip(*[part1(t, v0) for t in t1])
-x2, y2, z2 = zip(*[part2(t, v0) for t in t2])
-x3, y3, z3 = zip(*[part3(t, v0) for t in t3])
-
-# Affichage des graphes des coordonnées x, y et z en fonction du temps
-plt.figure(figsize=(12, 4))
+import csv
 
 
 
-"""
-xmax_ter = 20
-ymax_ter = 40
-t_graph_x = [0, 0, xmax_ter, xmax_ter, 0]
-t_graph_y = [0, ymax_ter, ymax_ter, 0, 0]
-plt.plot(t_graph_x, t_graph_y, '-k')
-"""
+
+def read_csv(path):
+    with open(path, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        x = []
+        y = []
+        z = []
+        t = []
+        for line in reader:
+            if (len(line) == 5):
+                tp1 = float(line[0])
+                tp2 = float(line[1])
+                dt = float(line[2])
+                nb = int(line[3])
+                reussite = int(line[4))
+            else :
+                x.append(float(line[0]))
+                y.append(float(line[1]))
+                z.append(float(line[2]))
+                t.append(float(line[3]))
+    return x, y, z, t1, t2, dt
+
+path = "./data.csv"
+
+x, y, z, t, tp1, tp2, dt, nb, reussite = read_csv(path)
+
+
+t1 = np.linspace(0, tp1, tp1/dt)
+t2 = np.linspace(tp1, tp2, (tp2-tp1)/dt)
+t3 = np.linspace(tp2, t[nb-1], 
+
+
 
 # Graphe des coordonnées x
 plt.subplot(131)
